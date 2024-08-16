86ab5f74c85006a3574a1e6ea961ac755fe8b7f8
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-08-16 15:44:47 -0400

    final work for glab 305-4-3

---
+++ src/main/java/org/example/Controller/App.java
@@ -0,0 +1,15 @@
+package org.example.Controller;
+
+public class App
+{    public static void main( String[] args )
+{
+    System.out.println( "Hello World!" );
+    EmployeeController e = new EmployeeController();
+//    e.createEmployeeTable();
+    //e.findEmployeeByname();
+    //e.findEmployeeById();
+    e.ShowOfficeCodes_AsDepartment();
+
+}
+}
+
